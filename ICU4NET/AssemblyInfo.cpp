52413a5ac36625fc9696a3f88a331cfdcb6fd043
niaher <nikbkk@gmail.com>
2015-04-04 16:35:17 +0700

    Feature: upgrade to new project.

--- ICU4NET/AssemblyInfo.cpp
+++ ICU4NET/AssemblyInfo.cpp
@@ -6,11 +6,6 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-//
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
 [assembly:AssemblyTitleAttribute("ICU4NET")];
 [assembly:AssemblyDescriptionAttribute("")];
 [assembly:AssemblyConfigurationAttribute("")];
@@ -19,22 +14,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyCopyrightAttribute("Copyright (c)  2010")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
 [assembly:AssemblyVersionAttribute("1.0.*")];
-
 [assembly:ComVisible(false)];
-
 [assembly:CLSCompliantAttribute(true)];
-
-[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
+//[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
