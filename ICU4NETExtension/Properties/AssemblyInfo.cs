47c9e5df0721af8348b517355f2c31b8d1467a69
unknown <nikitap@PCBKK-D6G3XY42.crp.unops.local>
2015-04-25 17:17:20 +0700

    Feature: updated icu-bin package.

--- ICU4NETExtension/Properties/AssemblyInfo.cs
+++ ICU4NETExtension/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("d6d3d7df-5a19-49a4-831e-ff0d572248bb")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
